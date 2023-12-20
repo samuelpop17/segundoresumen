105cd763e32028643453e748175dfe94c90c009a
samuelpop17 <samuelpop17@gmail.com>
2023-12-20 21:38:56 +0100

    dia31

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,12 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent{
 
+  
+
 
   public sendEmail(e: Event) {
     e.preventDefault();
     emailjs.sendForm('service_rv7bykp', 'template_wwpuvrj', e.target as HTMLFormElement, 'ExPSrGwO6SCA_y1bA')
       .then((result: EmailJSResponseStatus) => {
         console.log(result.text);
+        
       }, (error) => {
         console.log(error.text);
       });
@@ -24,6 +29,6 @@ export class HomeComponent{
       });
   }
   
-
+ 
 
 }
